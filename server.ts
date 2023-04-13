72e8a4f19b894a4017b3c6a168922c24ba4772c6
kry23 <infest.redstar@gmail.com>
2023-04-13 17:21:09 +0300

    node-ex-12 sol.

--- server.ts
+++ server.ts
@@ -2,7 +2,14 @@ import * as dotenv from "dotenv";
 import express, { json } from "express";
 import morgan from "morgan";
 dotenv.config();
-import joi from "joi";
+
+import {
+  addPlanet,
+  deletePlanet,
+  getAllPlanets,
+  getPlanetById,
+  updatePlanet,
+} from "./controllers/planets.js";
 const app = express();
 
 app.use(express.json());
@@ -10,77 +17,15 @@ app.use(morgan("dev"));
 
 const port = process.env.PORT_NUMBER || 5000;
 
-type Planet = {
-  id: number;
-  name: string;
-};
+app.get("/api/planets", getAllPlanets);
 
-type Planets = Planet[];
+app.get("/api/planets/:id", getPlanetById);
 
-let planets: Planets = [
-  {
-    id: 1,
-    name: "Earth",
-  },
-  {
-    id: 2,
-    name: "Mars",
-  },
-];
+app.post("/api/planets", addPlanet);
 
-const planetSchmea = joi.object({
-  id: joi.number().required(),
-  name: joi.string().required(),
-});
+app.delete("/api/planets/:id", deletePlanet);
 
-app.get("/api/planets", (req, res) => {
-  res.status(200).json(planets);
-});
-
-app.get("/api/planets/:id", (req, res) => {
-  const id: number = parseInt(req.params.id);
-  const planet: Planet | undefined = planets.find(
-    (planet) => planet.id === Number(id)
-  );
-  if (planet) {
-    res.status(200).json(planet);
-  } else {
-    res.status(404).send("Planet not found");
-  }
-});
-
-app.post("/api/planets", (req, res) => {
-  const { id, name } = req.body;
-  const planet: Planet = { id, name };
-
-  const validatedPlanet = planetSchmea.validate(planet);
-
-  if (validatedPlanet.error) {
-    res.status(400).json({ msg: "Please include a name or id" });
-  } else {
-    planets = [...planets, planet];
-    res.status(201).json({ msg: "planet added" });
-  }
-});
-
-app.delete("/api/planets/:id", (req, res) => {
-  const id: number = parseInt(req.params.id);
-  planets = planets.filter((planet) => planet.id !== id);
-
-  res.status(200).json({ msg: "Planet deleted" });
-});
-
-app.put("/api/planets/:id", (req, res) => {
-  const id: number = parseInt(req.params.id);
-  const { name } = req.body;
-
-  planets.find((planet) => {
-    if (planet.id === id) {
-      planet.name = name;
-      res.status(200).json({ msg: "Planet updated", planet });
-    }
-  });
-});
+app.put("/api/planets/:id", updatePlanet);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
